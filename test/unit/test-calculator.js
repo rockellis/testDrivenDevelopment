3008534f56506bb4252c0fccc8b1e35d9335029a
Brian Gallagher <brian.gallagher@feedhenry.com>
2016-05-10 11:59:22 +0100

    update

--- test/unit/test-calculator.js
+++ test/unit/test-calculator.js
@@ -7,17 +7,15 @@ describe('Test my simple calculator ', function () {
 
   it('should add the array of numbers ', function (done) {
 
-    var result = calculator.add([1, 2, 3, 4, 5]);
+    // TODO: Call the add function on the calculator module and assert the expected result
 
-    assert.equal(result, 15, "Addition does not match expected value");
     done();
   });
 
   it('should subtract the numbers correctly ', function (done) {
 
-    var result = calculator.subtract(10, 5);
+    // TODO: Call the subtract function on the calculator module and assert the expected result
 
-    assert.equal(result, 5, "Subtraction does not match expected value");
     done();
   });
 
